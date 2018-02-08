ffc50116b889e6bf7dcdf0568c7634815eaac1e8
Andreas Kern <code@andreaskern.de>
2018-02-08 21:25:22 +0100

    Create LoginViewController and LoginViewModel

---
+++ mvvmc/Login/LoginViewController.swift
@@ -0,0 +1,10 @@
+//
+//  LoginViewController.swift
+//  mvvmc
+//
+//  Created by Andreas Kern on 08.02.18.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {}
---
+++ mvvmc/Login/LoginViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  LoginViewModel.swift
+//  mvvmc
+//
+//  Created by Andreas Kern on 08.02.18.
+//
+
+import Foundation
+
+class LoginViewModel {}

0846e3635233b3526afe270a3f42dff3b1dcd05d
Andreas Kern <code@andreaskern.de>
2018-02-08 21:32:31 +0100

    Add viewModel to LoginViewController

--- mvvmc/Login/LoginViewController.swift
+++ mvvmc/Login/LoginViewController.swift
@@ -7,4 +7,16 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {}
+class LoginViewController: UIViewController {
+
+    let viewModel: LoginViewModel
+
+    init(viewModel: LoginViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

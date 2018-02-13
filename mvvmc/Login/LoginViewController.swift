4038ea480e48dd59ccba8f7b0fdaf73ac84c670a
Andreas Kern <code@andreaskern.de>
2018-02-13 12:35:59 +0100

    Set backgroundColor

--- mvvmc/Login/LoginViewController.swift
+++ mvvmc/Login/LoginViewController.swift
@@ -19,4 +19,10 @@ class LoginViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
 }
